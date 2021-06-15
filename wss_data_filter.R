a8731ff7c9e717f857c7a76bc176ae8ccf585505
Nicolas Steinmann <nicolas.steinmann@uzh.ch>
2021-06-15 14:37:15 +0200

    changed file names and deleted not needed data, wss_data_filter.R is the file to use now for analysis

--- wss_data_filter.R
+++ wss_data_filter.R
@@ -3,9 +3,9 @@ library(dplyr)
 library(sf)
 
 
+#Step 1: Reprojecting, Filtering and joining of deterrence device locations
 agenda <- ComputationalMovementAnalysisData::schreck_agenda
 locations <- ComputationalMovementAnalysisData::schreck_locations
-
 
 locations_sf <- st_as_sf(locations, coords = c('lon', 'lat'), crs = 4326)%>%
   
@@ -14,9 +14,11 @@ lv95_coords <- locations_sf%>%
   mutate(E = st_coordinates(.)[,1],
          N = st_coordinates(.)[,2])
 
-
-joined_tables <- left_join(lv95_coords, agenda, by = c("id" = "id"))%>%
+#Filtering by
+joined_tables_device_locs <- left_join(lv95_coords, agenda, by = c("id" = "id"))%>%
   filter(as.integer(month(as_date(datum_on))) >= 5  &  as.integer(month(as_date(datum_on))) <= 9)
 
+#
 
-write.csv(joined_tables, file = "data/wss_locations.csv")
+write.csv(joined_tables_device_locs, file = "data/device_locations_filtered.csv")
+write.csv(boar_locs_filtered, file = "data/boar_locations_filtered.csv")
