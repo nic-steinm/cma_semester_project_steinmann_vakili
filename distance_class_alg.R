686426d599dca7b0d2714bb8fadf1ad5f7a08fc3
Nicolas Steinmann <nicolas.steinmann@uzh.ch>
2021-06-18 17:12:14 +0200

    distance class calculation now working, analysis.R is being deleted as it malfuctions

--- distance_class_alg.R
+++ distance_class_alg.R
@@ -11,45 +11,40 @@ library(tmap)
 library(sp)
 library(ComputationalMovementAnalysisData)
 
-
+#Defining functions
 wss_distance <- function(x1, y1, x2, y2){
   sqrt((x1-x2)^2+(y1-y2)^2)
 } 
 
-r1 = 50
-r2 = 100
-r3 = 200
-wss_data <- read_delim("data/device_locations_filtered.csv", delim = ",")
-
-boar_data <- read_delim("data/boar_locations_filtered.csv", delim = ",")
-
-
-#area sizes of range classes in ha
-area_r1 <- round(((r1^2*pi) / 10000), 3)
-area_r2 <- round((((r2^2*pi) - area_r1) / 10000), 3)
-area_r3 <- round((((r3^2*pi) - (r2^2*pi)) / 10000), 3)
+distance_class <- function(wss_data, boar_data, r1, r2, r3){
+  
+  #area sizes of range classes in ha
+  area_r1 <- round(((r1^2*pi) / 10000), 3)
+  area_r2 <- round((((r2^2*pi) - area_r1) / 10000), 3)
+  area_r3 <- round((((r3^2*pi) - (r2^2*pi)) / 10000), 3)
   
   #add columns for the results of the calculations
-wss_data <- wss_data %>%
+  wss_data <- wss_data %>%
     add_column(r1 = r1,
                r2 = r2,
                r3 = r3,
                a_r1 = area_r1,
                a_r2 = area_r2,
                a_r3 = area_r3,
-               n_r1 = NA,
-               n_r2 = NA,
-               n_r3 = NA,
-               rho_r1 = NA,
-               rho_r2 = NA,
-               rho_r3 = NA)
+               n_r1 = 0,
+               n_r2 = 0,
+               n_r3 = 0,
+               rho_r1 = 0,
+               rho_r2 = 0,
+               rho_r3 = 0)
   
-
-for (i in 1:(nrow(wss_data))){
-    E_wss <- wss_data[i, 'E']
-    N_wss <- wss_data[i, 'N']
-    wss_start <- wss_data[i, 'datum_on']
-    wss_end <- wss_data[i, 'datum_off']
+  
+  for (i in 1:nrow(wss_data)){
+    E_wss <- wss_data$E[i]
+    N_wss <- wss_data$N[i]
+    wss_start <- wss_data$datum_on[i]
+    wss_end <- wss_data$datum_off[i]
+    n_days <- as.numeric(difftime(wss_end, wss_start, units = "days"))
     
     boar_data_filtered <- boar_data%>%
       filter(DatetimeUTC >= wss_start & DatetimeUTC <= wss_end)%>%
@@ -62,11 +57,28 @@ for (i in 1:(nrow(wss_data))){
     fixes_r3 <- boar_data_filtered%>%
       filter(dist > r2 & dist <= r3)
     
-    boar_data_filtered[i, 'n_r1'] <- nrow(fixes_r1)
-    boar_data_filtered[i, 'n_r2'] <- nrow(fixes_r2)
-    boar_data_filtered[i, 'n_r3'] <- nrow(fixes_r3)
+    #number of fixes in each range class and specified time interval
+    wss_data$n_r1[i] <- nrow(fixes_r1)
+    wss_data$n_r2[i] <- nrow(fixes_r2)
+    wss_data$n_r3[i] <- nrow(fixes_r3)
     
-    boar_data_filtered[i, 'rho_r1'] <- boar_data_filtered[i, 'n_r1']/area_r1
-    boar_data_filtered[i, 'rho_r2'] <- boar_data_filtered[i, 'n_r2']/area_r2
-    boar_data_filtered[i, 'rho_r3'] <- boar_data_filtered[i, 'n_r3']/area_r3
+    #boar density per hectare and day
+    wss_data$rho_r1[i] <- round((wss_data$n_r1[i]/area_r1)/n_days, 2)
+    wss_data$rho_r2[i] <- round((wss_data$n_r2[i]/area_r2)/n_days, 2)
+    wss_data$rho_r3[i] <- round((wss_data$n_r3[i]/area_r3)/n_days, 2)
+  }
 }
+
+#Defining distance parameters, loading data
+rad_1 = 100
+rad_2 = 250
+rad_3 = 2500
+
+wss_locs <- read_delim("data/device_locations_filtered.csv", delim = ",")
+
+boar_locs <- read_delim("data/boar_locations_filtered.csv", delim = ",")
+
+#Running the algorithm for switched on wildschweinschreck
+test <- distance_class(wss_locs, boar_locs, rad_1, rad_2, rad_3)
+
+#Running the algorithm for time before switched on
