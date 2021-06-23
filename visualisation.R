a7db52a60ff22d3fb3f085dac33331bfaa09236d
Nicolas Steinmann <nicolas.steinmann@uzh.ch>
2021-06-23 18:27:29 +0200

    implemented KDE visualisation

--- visualisation.R
+++ visualisation.R
@@ -1,7 +1,10 @@
 library(lubridate)
 library(dplyr)
 library(tibble)
+library(SpatialKDE)
+library(sp)
 library(sf)
+library(readr)
 library(tmap)
 
 wss_data <- read_delim("data/range_class_data.csv", delim = ",")
@@ -20,3 +23,33 @@ wss_buffer3 <- st_buffer(wss_geom, dist = 300)
 
 #bounding box for KDE
 boar_bb <- st_as_sfc(st_bbox(boar_geom))
+
+tmap_mode("view")
+
+tm_shape(wss_buffer3)+
+  tm_polygons(col = "red", alpha = 0.3) + 
+  tm_shape(wss_buffer2)+
+  tm_polygons(col = "blue", alpha = 0.3)+
+  tm_shape(wss_buffer1)+
+  tm_polygons(col = "green", alpha = 0.3)
+
+#KDE calculation
+cell_size <- 200
+band_width <- 150
+
+grid_boars <- boar_geom%>%
+  create_grid_hexagonal(cell_size = cell_size, side_offset = band_width)
+
+kde_boars <- boar_geom%>%
+  kde(band_width = band_width, kernel = "quartic", grid = grid_boars)%>%
+  filter(kde_value != 0)
+
+tm_shape(kde_boars)+
+  tm_fill(col = "kde_value", palette = "viridis", alpha = 0.3, style = "jenks")+
+  tm_borders(col = "NA")+
+tm_shape(wss_buffer3)+
+  tm_polygons(col = "red", alpha = 0.1) + 
+tm_shape(wss_buffer2)+
+  tm_polygons(col = "blue", alpha = 0.1)+
+tm_shape(wss_buffer1)+
+  tm_polygons(col = "green", alpha = 0.1)
