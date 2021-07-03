e1d07cdb8ba936095b6c98c3f4590ec5779ed23f
Nicolas Steinmann <nicolas.steinmann@uzh.ch>
2021-07-03 23:33:16 +0200

    worked on everything, looks much better now

--- visualisation.R
+++ visualisation.R
@@ -13,7 +13,7 @@ wss_data <- read_delim("data/range_class_data.csv", delim = ",")
 boar_data <- read_delim("data/boar_locations_filtered.csv", delim = ",")
 
 wss_data <- wss_data%>%
-  mutate(id = paste(as.character(X1), ":", " ", flurname, sep = ""))
+  mutate(id_name = paste(as.character(X1), ":", " ", flurname, sep = ""))
 
 #functions
 hex_grids <- function(geom, extent, cell_size){
@@ -40,8 +40,6 @@ hex_grids <- function(geom, extent, cell_size){
 
 
 
-
-
 #Choose your values
 wss_data
 
@@ -50,9 +48,6 @@ device_number <- 7
 
 #Select cell size in metres (be advised: cell size < 100 takes some time to calculate, < 50 not recommended)
 cell_size <- 100
-
-
-
 
 
 
@@ -80,7 +75,7 @@ end_date <- wss_geom$datum_off
 n_seconds <- as.numeric(difftime(end_date, start_date, units = "secs"))
 
  
-#honeycomb grid binning on and before on
+#honeycomb grid binning on status
 boar_geom_on <- st_as_sf(boar_data, coords = c("E", "N"), crs = 2056)
 boar_bbox <- st_as_sf(st_make_grid(boar_geom_on, n = 1))
 boar_geom_on <- filter(boar_geom_on, DatetimeUTC >= start_date & DatetimeUTC <= end_date)
@@ -89,6 +84,7 @@ boar_geom_on <- filter(boar_geom_on, DatetimeUTC >= start_date & DatetimeUTC <= 
 end_date <- start_date
 start_date <- start_date - n_seconds
 
+#honeycomb grid binning before on status
 boar_geom_bon <- boar_data%>%
   st_as_sf(coords = c("E", "N"), crs = 2056)%>%
   filter(DatetimeUTC >= start_date & DatetimeUTC <= end_date)
@@ -153,4 +149,61 @@ tm_shape(wss_geom)+
             "Mean step length r3 while active:" = "meansl_r3_on"
           ))
   
+
+
+# Visualisation #2 for general overview
+
+
+#loading function
+hex_grids <- function(geom, extent, cell_size){
   
+  grid <- extent%>%
+    create_grid_hexagonal(cell_size = cell_size)%>%
+    st_sf()%>%
+    mutate(id_hex = 1:n())%>%
+    dplyr::select(id_hex, geometry)
+  
+  grid_geom_joined <- st_join(geom, grid)
+  grid_count <- grid_geom_joined%>%
+    st_drop_geometry()%>%
+    count(id_hex)
+  
+  hex_grid_geom <- grid%>%
+    left_join(grid_count)#%>%
+  #filter(!is.na(n))
+  
+  hex_grid_geom[is.na(hex_grid_geom)] <- 0
+  
+  return(hex_grid_geom)
+}
+
+wss_geom <- wss_data%>%
+  st_as_sf(coords = c("E", "N"), crs = 2056)%>%
+  distinct(id, .keep_all= TRUE)
+
+
+boar_geom <- st_as_sf(boar_data, coords = c("E", "N"), crs = 2056)
+boar_bbox <- st_as_sf(st_make_grid(boar_geom, n = 1))
+
+grid_boars <- hex_grids(boar_geom, boar_bbox, 200)%>%
+  filter(n != 0)
+
+tmap_mode("view")
+tmap_style("cobalt")
+
+tm_shape(grid_boars)+
+  tm_fill(col = "n",
+          palette = "viridis",
+          style = "kmeans",
+          alpha = 0.3,
+          id = "n"
+          )+
+tm_shape(wss_geom)+
+  tm_dots(col = "red",
+          id = "id",
+          popup.vars = c(
+            "Region:" = "region",
+            "Place Name:" = "flurname",
+            "Crop:" = "kultur")
+          )
+          
